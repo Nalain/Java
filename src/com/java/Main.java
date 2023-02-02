280dda8a51acbae31850fca5fd7c94be4a46b1ab
Al <alain.yea.59@gmail.com>
2023-02-02 13:47:34 +0100

    Structure de controle

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,10 +1,43 @@
 package com.java;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("hello");
+		// Programme Main
+		Scanner sc = new Scanner(System.in);
+		
+		
+		// Variable
+		int var1 = 2;
+		int var2 = 3;
+		int var3 = 4;
+		int nombre = 0;
+		
+		
+		//Debut
+		System.out.print("Saisir une valeur ");
+		nombre = sc.nextInt();
+		
+		
+		if (nombre > 0) 
+			System.out.print("Valeur superieur à 0 "+ nombre);
+		else
+			System.out.print("Valeur inferieur à 0 "+ nombre);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//Fin
 	}
 
 }
